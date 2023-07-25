d1f1d6acf88c84a32e5a139384ffe06ccfb4bb8d
shalu <shalu@LAPTOP-V8JRTM1P>
2023-07-25 22:43:47 +1200

    Initial Commit

---
+++ Additional/src/logintest/BothCorrect.java
@@ -0,0 +1,33 @@
+package logintest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class BothCorrect {
+	
+	
+	@Test
+	@Parameters({"username","password"})
+	public void logindetails(String uname,String pword) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shalu\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(2000);
+		WebElement loginBtn =driver.findElement(By.xpath("//input[@name='username']"));
+		loginBtn.sendKeys(uname);
+		WebElement password = driver.findElement(By.xpath("//input[@name=\"password\"]"));
+		password.sendKeys(pword);
+		WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+		loginButton.click();
+			
+		
+		
+		
+	}
+
+}
